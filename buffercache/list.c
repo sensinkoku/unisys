5271a34131d8f3e267a1475c73b4e96ce38c02a7
ua483151 <ua483151@logex00.educ.cc.keio.ac.jp>
2016-10-21 14:14:30 +0900

    Add mycp and changed buffercache

---
+++ buffercache/list.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#define NHASH 4
+#define STAT_LOCKED 0x00000001
+#define STAT_VALID  0x00000002
+#define STAT_DWR    0x00000004
+#define STAT_KRDWR  0x00000008
+#define STAT_WAITED 0x00000010
+#define STAT_OLD    0x00000020
+  struct buf_header {
+    int blkno;
+    struct buf_header * hash_fp;
+    struct buf_header * hash_bp;
+    unsigned int stat;
+    struct buf_header *free_fp;
+    struct buf_header *free_bp;
+    char * cache_data;
+  };
+struct buf_header hash_head[NHASH];
+struct buf_header freehead;
+
+struct buf_header * search_hash(int blkno){
+  int h;
+  struct buf_header *p;
+  h = hash(blkno);
+  for (p = hash_head[h].hash_fp;p != &hash_head[h];p = p->hash_fp) if (p->blkno == blkno) return p;
+  return NULL;
+}
+struct buf_header * search_free(int blkno){
+  int h;
+  struct buf_header *p;
+  for (p = freehead.hash_fp;p != &freehead;p = p->hash_fp){
+    if(p->blkno == blkno) return p;
+  }
+  return NULL;
+}
+void insert_head(struct buf_header *h, struct buf_header *p) {
+  //p is insert node, h is head node.
+  int ha;
+  p->hash_bp = h;
+  p->hash_fp = h->hash_fp;
+  h->hash_fp->hash_bp = p;
+  h->hash_fp = p;
+}
+void remove_from_hash(struct buf_header * p){
+  p->hash_bp->hash_fp = p->hash_fp;
+  p->hash_fp->hash_bp = p->hash_bp;
+  p->hash_fp = p->hash_bp = NULL;
+}
+void remove_from_free(struct buf_header * p){
+  p->hash_bp->hash_fp = p->hash_fp;
+  p->hash_fp->hash_bp = p->hash_bp;
+  p->hash_fp = p->hash_bp = NULL;
+}
+
+struct buf_header * getblk(int blkno) {
+  struct buf_header * p;
+  while(p->blkno != blkno){
+    if(p = search_hash(blkno)){
+      if(p->stat == STAT_LOCKED){
+	//senirio 5
+	continue;
+      }
+      p->stat |= STAT_LOCKED;
+      
+    }
+
+    
+  }
+
+  
+  return NULL;
+}
+void brelse(struct buf_header *buffer) {
+  
+}
---
+++ buffercache/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+  int i;
+  for (i = 0; i !=1 && i < 10; i++){
+    printf("ok");
+  }
+  return 0;
+}
