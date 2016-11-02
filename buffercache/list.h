3cde097194a09419cf81af68c2de97a5a049b977
ua483151 <ua483151@y14e009.educ.cc.keio.ac.jp>
2016-11-02 16:15:27 +0900

    Made commands for mycommandline

--- buffercache/list.h
+++ buffercache/list.h
@@ -12,6 +12,7 @@
 //struct
 struct buf_header {
     int blkno;
+    int bufnum;
     struct buf_header * hash_fp;
     struct buf_header * hash_bp;
     unsigned int stat;
@@ -24,6 +25,9 @@ void insert_free(struct buf_header *, struct buf_header *, int );
 void remove_from_hash(struct buf_header * );
 struct buf_header * getblk(int blkno);
 void brelse(struct buf_header *buffer);
+void printhash(int i);
+void printfree();
+void buf_state_print(struct buf_header*);
 struct buf_header * search_hash(int blkno);
 //global v
 extern struct buf_header hash_head[NHASH];
@@ -87,28 +91,33 @@ struct buf_header * getblk(int blkno) {
     if((p = search_hash(blkno)) != NULL){
       if(p->stat & STAT_LOCKED){
       	//senirio 5
+	printf("Scenario5\n");
       	//sleep();
       	printf("Process goes to sleep\n");
       	return NULL;
       }
       //senario 1
+      printf("Scenario1\n");
       p->stat |= STAT_LOCKED;
       remove_from_free(p);
       return p;
     } else {
       if (freehead.free_fp == &freehead) {
         	//senario 4
+	printf("scenario4\n");
         	//sleep();
         	printf("Process goes to sleep\n");
         	return NULL;
       }
       if(fp->stat & STAT_DWR) {
       	//sinario3
+	printf("scenario3\n");
       	//write asynchronous
         fp = fp->free_fp;
       	continue;
       }
       //sinario2
+      printf("scenario2\n");
       remove_from_free(fp);
       remove_from_hash(fp);
       insert_head(&hash_head[blkno % NHASH],fp);
@@ -129,4 +138,34 @@ void brelse(struct buf_header *buffer) {
     insert_free(&freehead, buffer, 1);
   //lower_cpu_level();
   buffer->stat &= ~STAT_LOCKED;
+  return;
+}
+void buf_state_print(struct buf_header* s) {
+  int j;
+  char bitchar[6] = {'O','W','K','D','V','L'};
+  char stat[6] = {'-','-','-','-','-','-'};
+  for (j = 0; j < 6; j++) {
+    if (s->stat & 0x01 << j) stat[5-j] = bitchar[5-j];
+  }
+    printf("[%2d:%3d ", s->bufnum, s->blkno);
+    for (j = 0; j < 6; j++) printf("%c", stat[j]);
+    printf("]");
+  return;
+}
+void printhash(int i) {
+  struct buf_header * p;
+  printf("%d: ",i);
+  for (p = hash_head[i].hash_fp;p != &hash_head[i];p = p->hash_fp) {
+    buf_state_print(p);
+    printf(" ");
+  }
+  printf("\n");
+}
+void printfree() {
+  struct buf_header * p;
+  for (p = freehead.free_fp;p != &freehead;p = p->free_fp) {
+    buf_state_print(p);
+    printf(" ");
+  }
+  printf("\n");
 }
--- buffercache/main.c
+++ buffercache/main.c
@@ -1,4 +1,5 @@
 //#include <list.c>
+#include <stdlib.h>
 #include "testarg.h"
 #include "list.h"
 //#include <string.h>
@@ -12,7 +13,7 @@ void brelse_proc(int, char *[]);
 void reset_proc(int, char *[]);
 void quit_proc(int , char * []);
 void set_proc(int, char * []);
-
+void init();
 struct command_table{
   char * cmd;
   void (*func)(int, char *[]);
@@ -32,10 +33,10 @@ struct command_table{
 struct buf_header hash_head[NHASH];
 struct buf_header freehead;
 struct buf_header buffers[12];
-int blknos[12] = {3,35,99,98,50,10,17,5,97,28,4,64};
+int blknos[12] = {28,4,64,17,5,97,98,50,10,3,35,99};
 //int frees[6] = {3,5,4,28,97,10}
-int frees[6] = {0,7,10,9,8,5};
-
+int frees[6] = {9,4,1,0,5,8};
+int insertorder[12] = {2,1,0,5,4,3,8,7,6,11,10,9};
 
 int main(int argc, char *argv[])
 {
@@ -43,58 +44,99 @@ int main(int argc, char *argv[])
   char *av[16];
   char *input;
   struct command_table *p;
-  myinput(&ac, av, input);
-  for (p = cmd_tbl; p->cmd; p++)
-    if (strcmp(av[0], p->cmd) == 0) {
-      (*p->func)(ac, av);
-      break;
-    }
+  init();
+    while(1){
+      printf("$");
+    myinput(&ac, av, input);
+    for (p = cmd_tbl; p->cmd; p++)
+      if (strcmp(av[0], p->cmd) == 0) {
+	(*p->func)(ac, av);
+	break;
+      }
     if(p->cmd == NULL)
       fprintf(stderr, "unknown command: %s\n", av[0]);
+  }
   return 0;
 }
 void help_proc(int ac, char **av) {printf("help\n");return;}
-void init_proc(int ac, char **av) {
+void init(){
   //intialize ponters
   int i;
+  for(i = 0; i < NHASH; i++) {
+    hash_head[i].hash_fp = &hash_head[i];
+    hash_head[i].hash_bp = &hash_head[i];
+    hash_head[i].free_fp = &hash_head[i];
+    hash_head[i].free_fp = &hash_head[i];
+  }
+  freehead.hash_fp = &freehead;
+  freehead.hash_bp = &freehead;
+  freehead.free_fp = &freehead;
+  freehead.free_bp = &freehead;
   for (i = 0; i < 12; i++) {
     buffers[i].hash_fp = &buffers[i];
     buffers[i].hash_bp = &buffers[i];
     buffers[i].free_fp = &buffers[i];
     buffers[i].free_bp = &buffers[i];
     buffers[i].blkno = blknos[i];
-    insert_head(&hash_head[buffers[i].blkno % NHASH], &buffers[i]);
+    buffers[i].bufnum = i;
+    //insert_head(&hash_head[buffers[i].blkno % NHASH], &buffers[i]);
     buffers[i].stat = 0;
     buffers[i].stat |= STAT_VALID;
     buffers[i].stat |= STAT_LOCKED;
   }
+  for (i = 0; i < 12; i++) {
+    insert_head(&hash_head[buffers[insertorder[i]].blkno % NHASH], &buffers[insertorder[i]]);
+      }
   for (i = 0; i < 6; i++) {
     insert_free(&freehead, &buffers[frees[i]], 0);
     buffers[frees[i]].stat &= ~STAT_LOCKED;
   }
+  return;  
+}
+void init_proc(int ac, char **av) {
+  init();
   return;
 }
 void buf_proc(int ac, char **av){
-  int i, j;
-  char bitchar[6] = {'O','W','K','D','V','L'};
-  if (ac == 1) {
-    for (i = 0; i < 12; i++) {
-      char stat[6] = {'-','-','-','-','-','-'};
-      for (j = 0; j < 6; j++) {
-          if (buffers[i].stat & 0x01 << j) stat[j] = bitchar[j];
-      }
-    printf("[ %d: %d ", i, buffers[i].blkno);
-    for (j = 0; j < 6; j++) printf("%c", stat[j]);
-    printf("]\n");
-    }
+  int i;
+  for (i = 0; i < 12; i++) {
+    buf_state_print(&buffers[i]);
+    printf("\n");
   }
-
-  return;}
-void hash_proc(int ac, char **av){return;}
-void free_proc(int ac, char **av){printf("free\n");return;}
-void getblk_proc(int ac, char **av){return;}
+  return;
+}
+void hash_proc(int ac, char **av){
+  int i;
+  if (ac == 1) {
+    for (i = 0; i < NHASH; i++) printhash(i);
+    return;
+  }
+  for (i = 1;i < ac;i++ ){
+    printhash(atoi(av[i]));
+  }  
+  return;
+}
+void free_proc(int ac, char **av){
+  printfree();
+  return;
+}
+void getblk_proc(int ac, char **av){
+  if (ac != 2) printf("Usage: getblk <block number>\n");
+  else
+    getblk(atoi(av[1]));
+  return;
+}
 void brelse_proc(int ac, char **av){return;}
 void reset_proc(int ac, char **av){return;}
-void quit_proc(int ac, char **av) {return;}
+void quit_proc(int ac, char **av) {exit(0);return;}
 void set_proc(int ac, char **av){return;}
+/*void numbers(int ac, char **av, int *res) {
+  int i;
+  int num;
+  for (int i = 1; i < ac; i++) {
+    num = atoi(av[i]);
+    res[i-1] = 
+  }
 
+  return 0;
+  }*/
