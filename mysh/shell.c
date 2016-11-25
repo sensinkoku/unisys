19232ffa0a7cdc2ebfe7af6538eafdb5dcdc2ad1
ua483151 <ua483151@y14a046.educ.cc.keio.ac.jp>
2016-11-25 14:24:53 +0900

    changed mysh

--- mysh/shell.c
+++ mysh/shell.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #define LEN 100
 void myinput(int* ac, char ** av, char * input, int *ispipe, int *isredirect);
+static int redirect_check(char **av, int startindex, int endindex, int * inputplace, int * outputplace);
 int main(int argc, char *argv[])
 {
   int ac;
@@ -12,8 +13,10 @@ int main(int argc, char *argv[])
   char input[256];
   int ispipe[ac];
   int isredirect[ac];
+  
   int status;
   int i;
+  int pipenum = 0;
   while(1) {
     printf("$ ");
     myinput(&ac, av, input, ispipe, isredirect);
@@ -26,17 +29,42 @@ int main(int argc, char *argv[])
 	exit(0);
       } else {
 	int i;
-	int pipenum = 0;
 	int pipeplace;
 	int *pipefile[pipenum];
+	if (ispipe[0] != 0 || ispipe[ac] != 0) fprintf(stderr, "Input error\n");
 	for (i = 0; i < ac; i++) if (ispipe[i] == 1) pipenum++;
+	int j;
+	j = 0;
+	for (i = 0; i < pipenum+1; i++) {
+	  int start, end;
+	  int inputflag, outputflag;
+	  int redirect_in_index, redirect_out_index;
+	  //1 is pipe. 0 is stdin/out 2 is file.
+	  if (i == 0) start = 0;
+	  else start = j+1;
+	  while(ispipe[j] != 1 && j != ac-1) j++;
+	  if (ispipe[j] == 1) outputflag = 1;
+	  if (i != 0) inputflag = 1;
+	  if (i == 0) inputflag = 0;
+	  else inputflag = 1;
+	  // redirect check
+	  end = j-1;
+	  if (redirect_check(av, start,end,&redirect_in_index,&redirect_out_index) == -1) {
+	    fprintf(stderr,"Too many redirects\n");
+	    break;
+	  }
+	  if (redirect_in_index != -1) inputflag = 2;
+	  if (redirect_out_index != -1) outputflag = 2;
+	  
+	  
+	/*
 	if (ispipe[0] != 0 || ispipe[ac] != 0) fprintf(stderr, "Input error\n");
 	//change input and output stream
 	for (i = 0; i < pipenum; i++) pipe(pipefile[i]);
 	for (i = 0; i < ac-1; i++) {
 	  if (pipenum == 0) {
 
-	  }
+	  }*/
 	  //	  while (ispipe[i] != 1) if (i != )i++;
 	  
 	}
@@ -54,7 +82,33 @@ int main(int argc, char *argv[])
   }
   return 0;
 }
-
+static int redirect_check(char **av, int startindex, int endindex, int * inputplace, int * outputplace) {
+  int i;
+  int inp_num, oup_num;
+  inp_num = 0; oup_num = 0;
+  *inputplace = -1;
+  *outputplace = -1;
+  for (i = startindex; i < endindex+1; i++) {
+    if (strcmp(av[i], "<") == 0) {
+      inp_num++;
+      *inputplace = i;
+      if (i == endindex) {
+	fprintf(stderr, "Illegal usage of redirect.\n");
+	return -1;
+      }
+    }
+    if (strcmp(av[i], ">") == 0){
+       oup_num++;
+      *outputplace = i;
+      if (i == startindex) {
+	fprintf(stderr, "Illegal usage of redirect.\n");
+	return -1;
+      }
+  }
+  if (inp_num >= 2 || oup_num >= 2) {
+    fprintf(stderr, "Illegal usage of redirect.\n");
+  }
+}
 void myinput(int* ac, char ** av, char * input, int *ispipe, int *isredirect) {
   fgets(input, 250, stdin);
   if (strlen(input)>=1) input[strlen(input) - 1] = '\0';
