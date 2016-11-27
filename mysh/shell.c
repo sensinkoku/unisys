ab30a3b4716d44fd35246de17507c24368f2058f
sensinkoku <nmoari@gmail.com>
2016-11-27 22:56:54 +0900

    リダイレクト処理を追加

--- mysh/shell.c
+++ mysh/shell.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -6,14 +8,15 @@
 #define LEN 100
 void myinput(int* ac, char ** av, char * input, int *ispipe, int *isredirect);
 static int redirect_check(char **av, int startindex, int endindex, int * inputplace, int * outputplace);
+static int pipefdopen(int x, int y, int array[x][y]);
 int main(int argc, char *argv[])
 {
+	printf("test");
   int ac;
   char *av[16];
   char input[256];
   int ispipe[ac];
   int isredirect[ac];
-  
   int status;
   int i;
   int pipenum = 0;
@@ -25,75 +28,131 @@ int main(int argc, char *argv[])
         char *  home = getenv("HOME");
         if (ac == 1 || strcmp(av[1], "~") == 0) chdir(home);
         else chdir(av[1]);
-      } else if (strcmp(av[ac-1],"exit")) {
+      } else if (strcmp(av[ac-1],"exit") == 0) {
 	exit(0);
       } else {
 	int i;
 	int pipeplace;
-	int *pipefile[pipenum][2];
-	if (ispipe[0] != 0 || ispipe[ac] != 0) fprintf(stderr, "Input error\n");
+	int pipefile[pipenum][2];
+	printf("\n%d %d\n",ac, pipenum);
+	for (i = 0; i < ac; i++) printf("this%d this",ispipe[i]);
+	if (ispipe[0] != 0 || ispipe[ac-1] != 0) fprintf(stderr, "Input error\n");
 	for (i = 0; i < ac; i++) if (ispipe[i] == 1) pipenum++;
 	int j;
 	j = 0;
-	
+	printf("pipenum %d\n",pipenum);
 	for (i = 0; i < pipenum+1; i++) {
 	  int start, end;
+	  int comstart, comend;
 	  int inputflag, outputflag;
 	  int redirect_in_index, redirect_out_index;
+	  int fd1, fd2;
 	  //1 is pipe. 0 is stdin/out 2 is file.
 	  if (i == 0) start = 0;
 	  else start = j+1;
 	  while(ispipe[j] != 1 && j != ac-1) j++;
 	  if (ispipe[j] == 1) outputflag = 1;
+	  else outputflag = 0;
 	  if (i != 0) inputflag = 1;
 	  if (i == 0) inputflag = 0;
 	  else inputflag = 1;
 	  // redirect check
-	  end = j-1;
+	  if (outputflag == 1) end = j-1;
+	  else end = j;
 	  if (redirect_check(av, start,end,&redirect_in_index,&redirect_out_index) == -1) {
 	    fprintf(stderr,"Too many redirects\n");
 	    break;
 	  }
 	  if (redirect_in_index != -1) inputflag = 2;
 	  if (redirect_out_index != -1) outputflag = 2;
-	  if (pipefdopen(int **pfd, pipenum) == -1) {
+	  if (pipefdopen(pipenum,2,pipefile) == -1) {
 	    fprintf(stderr, "pipe fd miss\n");
 	  }
-
-
-	  
-	/*
-	if (ispipe[0] != 0 || ispipe[ac] != 0) fprintf(stderr, "Input error\n");
-	//change input and output stream
-	for (i = 0; i < pipenum; i++) pipe(pipefile[i]);
-	for (i = 0; i < ac-1; i++) {
-	  if (pipenum == 0) {
-
-	  }*/
-	  //	  while (ispipe[i] != 1) if (i != )i++;
-	  
+	  comend = end;
+	  comstart = start;
+	  if (fork() == 0) {
+	    switch (inputflag) {
+	    case 0:{
+	    	fprintf(stderr,"input-0\n");
+	    break;
+	    }
+	    case 1: {
+	    fprintf(stderr,"input-1\n");
+	      close(0);
+	      dup(pipefile[i-1][0]);
+	      break;
+	    }
+	    case 2: {
+	    	fprintf(stderr,"input-2\n");
+	      comend = redirect_in_index + 1;
+	      fd1 = open(av[redirect_in_index+1],O_RDONLY, 0644);
+	      close(1);
+	      dup(fd1);
+	      close(fd1);
+	      break;
+	    }
+	    default:
+	    fprintf(stderr,"input-def\n");
+	      break;
+	    }
+	    switch (outputflag) {
+	    case 0: {
+	    	fprintf(stderr,"output-0\n");
+	      break;
+	    }
+	    case 1: {
+	    		fprintf(stderr,"output-1\n");
+	      close(0);
+	      dup(pipefile[i][1]);
+	      break;
+	    }
+	    case 2: {
+	    		fprintf(stderr,"output-2\n");
+	      comend = redirect_out_index - 1;
+	      fd2 = open(av[redirect_out_index+1],O_WRONLY|O_CREAT|O_TRUNC, 0644);
+	      close(1);
+	      dup(fd2);
+	      close(fd2);
+	      break;
+	    }
+	    default:
+	    	fprintf(stderr,"output-def\n");
+	      break;
+	    }
+	    //exe command
+	    int comlength = comend-comstart+1;
+	    char * exein[comlength+1];
+	    int k;
+	    for (k = 0; k < comlength; k++) {
+	      exein[k] = av[comstart+k];
+	    }
+	    fprintf (stderr,"comlength is %d\n", comend-comstart+1);
+	    exein[comlength] = NULL;
+	    execvp(exein[0],exein);
+	    //change fg group
+	    //signalcheck
+	    // 
+	  }
+	  	    printf ("comlength is %d\n", comend-comstart+1);
+      if (strcmp(av[ac-1],"&") != 0)  wait(&status);
+	}//for kakko
+	int k;
+	for (k = 0; k < pipenum;k++) {
+	  close(pipefile[k][0]);
+	  close(pipefile[k][1]);
 	}
-        pid_t p = fork();
-        if (p == 0) {
-          if (ac >= 1) av[ac] = NULL;
-          execvp(av[0],av);
-        } else {
-          if (strcmp(av[ac-1],"&") != 0) {
-            wait(&status);
-          }
-        }
-      }
+    }
     }
   }
   return 0;
 }
-static int pipefdopen(int ** pfds, int pipenum) {
+static int pipefdopen(int x,int y, int array[x][y]) {
   int i;
-  if (pipenum == 0) {
+  if (x == 0) {
     return 2;
   }
-  for (i = 0; i < pipenum; i++) {
-    if(pipe(pfds[i]) == -1) {
+  for (i = 0; i < x; i++) {
+    if(pipe(array[x]) == -1) {
       return -1;
     }
   }
@@ -114,7 +173,7 @@ static int redirect_check(char **av, int startindex, int endindex, int * inputpl
 	return -1;
       }
     }
-    if (strcmp(av[i], ">") == 0){
+    if (strcmp(av[i], ">") == 0) {
        oup_num++;
       *outputplace = i;
       if (i == startindex) {
@@ -124,7 +183,8 @@ static int redirect_check(char **av, int startindex, int endindex, int * inputpl
   }
   if (inp_num >= 2 || oup_num >= 2) {
     fprintf(stderr, "Illegal usage of redirect.\n");
-  }
+  	}
+	}
 }
 void myinput(int* ac, char ** av, char * input, int *ispipe, int *isredirect) {
   fgets(input, 250, stdin);
@@ -152,9 +212,9 @@ void myinput(int* ac, char ** av, char * input, int *ispipe, int *isredirect) {
     isredirect[i] = 0;
   }
   for (i = 0; i < *ac; i++) {
-    if (strcmp(av[i],"|")) ispipe[i] = 1;
-    if (strcmp(av[i], ">")) ispipe[i] = 2;
-    if (strcmp(av[i], "<")) ispipe[i] = 3;
+    if (strcmp(av[i],"|") == 0) ispipe[i] = 1;
+    if (strcmp(av[i], ">") == 0) ispipe[i] = 2;
+    if (strcmp(av[i], "<") == 0) ispipe[i] = 3;
   }
   return;
 }
